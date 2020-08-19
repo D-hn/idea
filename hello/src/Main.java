bbf64b8493edefa5ff3c221b5202192193a90c4e
dhn <313655693@qq.com>
2020-08-19 09:53:51 +0800

    测试

---
+++ hello/src/Main.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+     int a =1;
+    public static void main(String[] args) {
+        Student c = new Student();
+        Student a = new Student();
+        Student b = a;
+        System.out.println(b.toString());
+        a = c;
+        System.out.println(a.toString() + b.toString());
+        Map<String,String> m=new HashMap<String,String>();
+m.put("1","qq");
+    }
+    @Test
+    public  void t1(){
+        a=2;
+        System.out.println(a);
+        t2();
+    }
+    @Test
+    public  void t2(){
+
+        System.out.println(a);
+    }
+}
